8aef43915427d4f5606a6fc97888ef8de64ac308
muthu <mkandanmk7@gmail.com>
2021-10-26 14:03:12 +0530

    reset initiated

--- Services/resetPass.service.js
+++ Services/resetPass.service.js
@@ -11,6 +11,13 @@ const service = {
     // res.status(200).send({ key: "user result done", result: userExist });
     //check exist or not
     if (!userExist) res.status(400).send("user doesn't exists");
+    if (userExist.resetToken) {
+      let data = await mongo.register.update(
+        { email: userExist.email },
+        { $unset: { resetToken: 1, resetExpire: 1 } }
+      );
+      console.log(data);
+    }
   },
 };
 
