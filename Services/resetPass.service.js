9186ca1d7216ce630762ea65ccd1ba29371f1a44
muthu <mkandanmk7@gmail.com>
2021-10-28 21:46:50 +0530

    reset token done

--- Services/resetPass.service.js
+++ Services/resetPass.service.js
@@ -11,6 +11,9 @@ const service = {
     // res.status(200).send({ key: "user result done", result: userExist });
     //check exist or not
     if (!userExist) res.status(400).send("user doesn't exists");
+
+    //if user reset token was not used , remove the reset token unset
+
     if (userExist.resetToken) {
       let data = await mongo.register.update(
         { email: userExist.email },
@@ -18,6 +21,24 @@ const service = {
       );
       console.log(data);
     }
+
+    // create string for hashing pass;
+    let token = crypto.randomBytes(32).toString("hex");
+    let hashToken = await bcrypt.compare(token, Number(12));
+    console.log(token, "hashTOken: ", hashToken);
+    //expiry timing for 1 hour
+    let expiry = new Date(Date.now() + 1 * 3600 * 1000);
+    console.log(expiry);
+
+    //updating resetToken and expiry time to user details;
+    let data = await mongo.register.findOneAndUpdate(
+      { email: userExist.email },
+      { $set: { resetToken: hashToken, resetExpire: expiry } },
+      { ReturnDocument: "after" }
+    );
+    console.log(data);
+
+    res.status(200).send({ message: "Link sent to email" });
   },
 };
 
